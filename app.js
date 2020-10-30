0e9a4a05dd4e979e911b38296bf3c9fb3ab19da0
Isra <israel_2896@hotmail.com>
2020-10-29 18:11:22 -0600

    Motores de vista

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require("express");
 //ejecuta expresss
+
 var app = express();
+//carga el modulo express
+
+app.set("view engine", "jade");
 //especifica la ruta y mandar una funcion que recibe dos paramteros,
+
 //una de la peticion una de respuesta
 app.get("/", function(req,res){
-    res.send("Hola mundo");
+    res.render("index", {Hola: "israel "});
 });
 //ejecuta el server con el puerto 8080
 app.listen(8080);
