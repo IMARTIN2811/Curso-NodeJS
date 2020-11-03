340e114f56610f4e97b7aa9fc8c05cbaac3d8326
Isra <israel_2896@hotmail.com>
2020-11-03 13:14:03 -0600

    Conexion a MongoDB con Mongoose

--- LayoutJADE/app.js
+++ LayoutJADE/app.js
@@ -2,6 +2,32 @@ var express = require("express");
 var app = express();
 //se importa la libreria
 var bodyParser = require("body-parser");
+//se importa la libreria
+var mongoose = require("mongoose");
+//Se declara el esquema
+var Schema = mongoose.Schema;
+
+//Se hace la conexion
+mongoose.connect("mongodb://localhost");
+var db = mongoose.connection
+//Si la conexion en incorecta marca un errro
+db.on('error', function(err){
+  console.log('connection error', err)
+})
+//Verifica la conexion a BD
+db.once('open', function(){
+  console.log('Connection to DB successful')
+})
+
+//Se genera los objetos del documento
+var userSchemaJSON = {
+    email:String,
+    password:String
+};
+//Crea el esquema y pasa la estrucutura del documento
+var user_Schema = new Schema(userSchemaJSON);
+//Se crea el modelo para establecer la conexion
+var User = mongoose.model("User",user_Schema);
 
 //se declara el middleware y carga los archvos estaticos
 app.use("/estatico",express.static('public'));
@@ -19,14 +45,21 @@ app.get("/", function(req,res){
 //especifica la ruta y mandar una funcion que recibe dos paramteros,
 //una de la peticion una de respuesta
 app.get("/login", function(req,res){
-    res.render("login");
+    User.find(function(err,doc){
+        console.log(doc);
+        res.render("login");
+    });
 });
 
 //Se declara metodo post y envia los datos 
 app.post("/users", function(req,res) {
-    console.log("Contraseña:"+ req.body.password);
-    console.log("Email:"+ req.body.email);
-    res.send("Recibimos tus datos");
+    //console.log("Contraseña:"+ req.body.password);
+    //console.log("Email:"+ req.body.email);
+    var user = new User({email: req.body.email, password: req.body.password});
+    //guarda los datos
+    user.save(function(){
+        res.send("Guardamos tus datos");
+    });
 });
 
 app.listen(8080);
