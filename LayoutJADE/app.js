175665fad083e4aed32718fc163f2133a87906dd
Isra <israel_2896@hotmail.com>
2020-11-02 18:15:24 -0600

    Lectura de parametros

--- LayoutJADE/app.js
+++ LayoutJADE/app.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var app = express();
+//se importa la libreria
+var bodyParser = require("body-parser");
+
 //se declara el middleware y carga los archvos estaticos
 app.use("/estatico",express.static('public'));
 app.use(express.static('assets'));
-
+//para peticiones aplication/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "jade");
+
 //especifica la ruta y mandar una funcion que recibe dos paramteros,
 //una de la peticion una de respuesta
 app.get("/", function(req,res){
@@ -14,6 +20,13 @@ app.get("/", function(req,res){
 //una de la peticion una de respuesta
 app.get("/login", function(req,res){
     res.render("login");
+});
+
+//Se declara metodo post y envia los datos 
+app.post("/users", function(req,res) {
+    console.log("Contraseña:"+ req.body.password);
+    console.log("Email:"+ req.body.email);
+    res.send("Recibimos tus datos");
 });
 
 app.listen(8080);
