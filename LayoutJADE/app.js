8da6062b4ad712df162e3c024cc8360eb2a68ac3
Isra <israel_2896@hotmail.com>
2020-11-04 16:28:42 -0600

    Validaciones personalizadas

--- LayoutJADE/app.js
+++ LayoutJADE/app.js
@@ -57,7 +57,8 @@ app.post("/users", function(req,res) {
     //console.log("Email:"+ req.body.email);
     var user = new User({email: req.body.email, 
                             password: req.body.password,
-                            password_confirmation: req.body.password_confirmation
+                            password_confirmation: req.body.password_confirmation,
+                            username: req.body.username
                         });
     console.log(user.password_confirmation);
     
--- LayoutJADE/models/user.js
+++ LayoutJADE/models/user.js
@@ -10,13 +10,28 @@ var emailMatch = [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,"Coloca un ema
 //Crea el esquema y pasa la estructura del documento
 var useSchema = new Schema({
     name: String,
+    lastName: String,
     //Hace las validaciones
-    userName: {type: String,required:true,maxlength:[50,"El usuario es muy grande"]},
-    password: {type: String,minlength:[8,"El password es muy corto"]},
-    age : {type: Number, min: [5,"La edad no puede ser menor que 5"], max:[100,"La edad no puede ser mayor que 100"] },
-    email: {type: String, required: "El correo el obligatorio",match: emailMatch},
+    userName: {
+        type: String,required:true,maxlength:[50,"El usuario es muy grande"]
+    },
+    password: {
+        type: String,minlength:[8,"El password es muy corto"],
+        validate: { validator: function(p) {
+            return this.password_confirmation == p;
+    }, 
+        message: "Las contraseñas no son iguales" }
+    },
+    age : {
+        type: Number, min: [5,"La edad no puede ser menor que 5"], max:[100,"La edad no puede ser mayor que 100"] 
+    },
+    email: {
+        type: String, required: "El correo el obligatorio",match: emailMatch
+    },
     dateOfBirth: Date,
-    sex: {type: String,enum:{values: posiblesValores, message:"Opción no válida"} }
+    sex: {
+        type: String,enum:{values: posiblesValores, message:"Opción no válida"} 
+    }
 });
 
 //se crea el virtual
