d5aaa521ae4a5ed1fd389554afaa854b9566a939
Isra <israel_2896@hotmail.com>
2020-11-05 17:32:01 -0600

    Manejo de sesiones

--- LayoutJADE/app.js
+++ LayoutJADE/app.js
@@ -6,14 +6,20 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 //Se declara el esquema
 var Schema = mongoose.Schema;
+//importa el archivo de validaciones
+//var User = require("./models/user").User;
+//se declara el session
+var session = require("express-session");
 
 //Se hace la conexion
 mongoose.connect("mongodb://localhost");
 var db = mongoose.connection
+
 //Si la conexion en incorecta marca un errro
 db.on('error', function(err){
   console.log('connection error', err)
 })
+
 //Verifica la conexion a BD
 db.once('open', function(){
   console.log('Connection to DB successful')
@@ -24,6 +30,7 @@ var userSchemaJSON = {
     email:String,
     password:String
 };
+
 //Crea el esquema y pasa la estrucutura del documento
 var user_Schema = new Schema(userSchemaJSON);
 //Se crea el modelo para establecer la conexion
@@ -37,11 +44,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "jade");
 
+//Se define el middleware de la sesion
+app.use(session({
+    secret: "123byuhbsdah12ub",
+    resave: false,
+    saveUninitialized: false
+}));
+
 //especifica la ruta y mandar una funcion que recibe dos paramteros,
 //una de la peticion una de respuesta
 app.get("/", function(req,res){
+    console.log(req.session.user_id)
     res.render("index");
 });
+
 //especifica la ruta y mandar una funcion que recibe dos paramteros,
 //una de la peticion una de respuesta
 app.get("/signup", function(req,res){
@@ -50,6 +66,7 @@ app.get("/signup", function(req,res){
         res.render("signup");
     });
 });
+
 //especifica la ruta y mandar una funcion que recibe dos paramteros,
 //una de la peticion una de respuesta
 app.get("/login", function(req,res){
@@ -60,8 +77,9 @@ app.get("/login", function(req,res){
 app.post("/sessions", function(req,res) {
     //Se declara el metodo find y devuelve los documentos
     //find: te trae multiples documentos, findOne: te trae un solo documento
-    User.findOne({email: req.body.email,password: req.body.password },function(err,docs){
-        console.log(docs);
+    User.findOne({email: req.body.email,
+                  password: req.body.password },function(err,user){
+        req.session.user_id = user._id;
         res.send("Hola mundo");
     });
 });
