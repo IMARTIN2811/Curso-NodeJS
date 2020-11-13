7447d13bd9ef7c7fa690f55f397327e5531353e1
Isra <israel_2896@hotmail.com>
2020-11-12 19:18:58 -0600

    Refactorizacion de codigo y modificacion de archivo modelo

--- LayoutJADE/middlewares/find_images.js
+++ LayoutJADE/middlewares/find_images.js
@@ -3,15 +3,17 @@ var Imagen = require("../models/imagenes");
 //se declara el middleware y se exporta
 module.exports = function(req,res,next) {
     //busca la imagen por medio de la id
-    Imagen.findById(req.params.id,function(err,imagen) {
-        //valida si encuentra la imagen 
-        if (imagen != null) {
-            console.log("Encontre la imagen"+ imagen.title);
-            res.locals.imagen = imagen;
-            next();
-        }else{
-            //sino encuentra la imagen lo redirigira a la ruta /app
-            res.redirect("/app");
-        }        
-    })
+    Imagen.findById(req.params.id)
+        .populate("creator")
+        .exec(function(err,imagen) {
+            //valida si encuentra la imagen 
+            if (imagen != null) {
+                //console.log("Encontre la imagen"+ imagen.creator);
+                res.locals.imagen = imagen;
+                next();
+            }else{
+                //sino encuentra la imagen lo redirigira a la ruta /app
+                res.redirect("/app");
+            }        
+        })
 } 
--- LayoutJADE/models/imagenes.js
+++ LayoutJADE/models/imagenes.js
@@ -5,7 +5,10 @@ var Schema = mongoose.Schema;
 
 //se declara la esquema de la imagen
 var imgSchema = new Schema({
-    title : { type: String, required: true }
+    title : { type: String, required: true },
+    //se agrega un nuevo campo al esquema
+    //especifica que el id de otro objeto que es User
+    creator: { type: Schema.Types.ObjectId, ref: "User" }
 });
 
 var Imagen = mongoose.model("Imagen",imgSchema);
